4b7e9c39d6bc7ea3a7e27d919915e70ef513e242
zurgur <harryfreyr@gmail.com>
2018-04-09 16:54:10 +0000

    reducer fra ola

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,16 +1,47 @@
 import {
   LOGIN_REQUEST,
-  /* todo fleiri actions */
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGIN_LOGOUT,
 } from '../actions/auth';
 
 const initialState = {
+  isFetching: false,
+  isAuthenticated: user ? true : false,
+  user,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-
-    /* todo setja upp reducer */
-
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        isAuthenticated: action.isAuthenticated,
+      };
+    case LOGIN_SUCCESS:
+    console.log(action)
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        isAuthenticated: action.isAuthenticated,
+        user: action.user,
+        message: action.message,
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        isAuthenticated: action.isAuthenticated,
+        message: action.message
+      };
+    case LOGIN_LOGOUT:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        isAuthenticated: action.isAuthenticated,
+        user: action.user,
+      }
     default:
       return state;
   }
