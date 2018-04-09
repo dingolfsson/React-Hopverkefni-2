d52d487a705cd9a322c6570ab8fc4aff479ae139
quynh <npquynhdo@gmail.com>
2018-04-09 19:15:19 +0000

    add url

--- src/api.js
+++ src/api.js
@@ -14,12 +14,40 @@ async function get(endpoint) {
   if (token) {
     options.headers['Authorization'] = `Bearer ${token}`;
   }
-
   /* todo framkvæma get */
+  const response = await fetch(url);
+  const result = await response.json();
+
+  return { result, status: response.status };
 }
 
+
 /* todo aðrar aðgerðir */
+async function post(endpoint, data) {
+  const url = `${baseurl}${endpoint}`;
+
+  const options = {
+    body: JSON.stringify(data),
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'POST',
+  };
+
+  const response = await fetch(url, options);
+  const result = await response.json();
+
+  return { result, status: response.status };
+}
+
+async function patch(endpoint, data) {
+  const url = `${baseurl}${endpoint}`;
+
+  //TODO 
+}
 
 export default {
   get,
+  post,
+  patch,
 };
