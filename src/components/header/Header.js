42833cf4e4bad8493ed16c9b06f2f991df0e948c
dingolfsson <dai5@hi.is>
2018-04-10 16:34:33 +0000

    Login header, home and login page fixed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,15 +5,35 @@ import { Link } from 'react-router-dom';
 
 import Button from '../button';
 
+import { loginUser, logoutUser } from '../../actions/auth';
+
 import './Header.css';
 
 class Header extends Component {
+
+  handleLogout = (e) => {
+    const { dispatch } = this.props;
+    dispatch(logoutUser());
+  }
 
   onClick = (e) => {
     console.log('leita');
   }
 
   render() {
+    const { name } = this.props;
+    if (this.props.auth.isAuthenticated) {
+      return (
+        <header className="header">
+          <h1 className="header__heading"><Link to="/">Bókasafnið</Link></h1>
+  
+          {/* ætti samt frekar heima í sér component */}
+          <Button onClick={this.onClick}>Leita</Button>
+          <Link to="/profile">{ name }</Link>
+          <Button onClick={this.handleLogout}>Logout</Button>
+        </header>
+      );
+    }
     return (
       <header className="header">
         <h1 className="header__heading"><Link to="/">Bókasafnið</Link></h1>
@@ -30,6 +50,7 @@ class Header extends Component {
 const mapStateToProps = (state) => {
   return {
     auth: state.auth,
+    name: state.auth.user ? state.auth.user.name : '',
   }
 }
 
